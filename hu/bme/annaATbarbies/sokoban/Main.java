5d4bba881721d97e95fd36beaf99c713d10bd108
Iványi Béla <ivanyi.beci@gmail.com>
2018-03-04 12:02:22 +0100

    .gitignore not needed

--- hu/bme/annaATbarbies/sokoban/Main.java
+++ hu/bme/annaATbarbies/sokoban/Main.java
@@ -3,6 +3,6 @@ package hu.bme.annaATbarbies.sokoban;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello World!");
     }
 }
