ab8a791a8ffc3089958d613924173092180df88d
Iványi Béla <ivanyi.beci@gmail.com>
2018-03-04 12:54:01 +0100

    Update Direction.java

--- hu/bme/annaATbarbies/sokoban/model/Direction.java
+++ hu/bme/annaATbarbies/sokoban/model/Direction.java
@@ -1,5 +1,15 @@
 package hu.bme.annaATbarbies.sokoban.model;
 
 public enum Direction {
-    UP, RIGHT, DOWN, LEFT
+    UP, RIGHT, DOWN, LEFT;
+    
+    public static Direction oppositeDirection(Direction dir) {
+        switch(dir) {
+            case UP: return DOWN;
+            case RIGHT: return LEFT;
+            case DOWN: return UP;
+            case LEFT: return RIGHT;
+            default: return UP;
+        }
+    }
 }
