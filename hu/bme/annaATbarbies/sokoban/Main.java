3f10e2898fe7c6beb8c598c83afcd6471939cfe5
remenyigergely <remenyig@gmail.com>
2018-03-08 22:36:33 +0100

    valamit megváltoztattam a masterben. És most nem tudok kilépni:( Mert most jöttem rá, hogy ez a hülye vim >((

--- hu/bme/annaATbarbies/sokoban/Main.java
+++ hu/bme/annaATbarbies/sokoban/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Hello Bela!");
         System.out.println("Hello Gergo! Oke, ez a masodik probalkozasom az eclipse IDE-bol.");
         System.out.println("Hello Balazs!");
-	System.out.println("Hello Jozsi!");
+        System.out.println("Hello Jozsi!");
+        System.out.println("Ez csak a Gergo branchen jelenhet meg!");
     }
 }
