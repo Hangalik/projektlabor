3f4e1adafe7d005b5b746a3cafb13e8d552a8d3a
remenyigergely <remenyig@gmail.com>
2018-03-08 22:06:02 +0100

    Megvaltoztattam a Main.java-t. Gergo voltam.

--- hu/bme/annaATbarbies/sokoban/Main.java
+++ hu/bme/annaATbarbies/sokoban/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello Bela!");
-        System.out.println("Hello Gergo!");
+        System.out.println("Hello Gergo! Oke, ez a masodik probalkozasom az eclipse IDE-bol.");
         System.out.println("Hello Balazs!");
     }
 }
