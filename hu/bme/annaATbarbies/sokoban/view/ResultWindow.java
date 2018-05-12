cc1958598264fa49a990384f9478d2e00e582eca
Iványi Béla <ivanyi.beci@gmail.com>
2018-05-12 13:03:30 +0200

    Játékosok rendezésének megvalósítása.

--- hu/bme/annaATbarbies/sokoban/view/ResultWindow.java
+++ hu/bme/annaATbarbies/sokoban/view/ResultWindow.java
@@ -1,10 +1,16 @@
 package hu.bme.annaATbarbies.sokoban.view;
 
+import hu.bme.annaATbarbies.sokoban.model.Floor;
+import hu.bme.annaATbarbies.sokoban.model.pushable.Controller;
+
 import javax.swing.*;
+import java.util.Collections;
+import java.util.List;
 
 public class ResultWindow extends JFrame {
 
     public void createAndShow() {
-
+        List<Controller> workers = Floor.getInstance().getAllWorkers();
+        Collections.sort(workers);
     }
 }
