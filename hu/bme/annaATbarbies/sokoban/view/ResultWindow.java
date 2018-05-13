9d70468c9416755c63bdfe340a9bd9353a126b62
Iványi Béla <ivanyi.beci@gmail.com>
2018-05-13 16:32:12 +0200

    Result

--- hu/bme/annaATbarbies/sokoban/view/ResultWindow.java
+++ hu/bme/annaATbarbies/sokoban/view/ResultWindow.java
@@ -2,6 +2,7 @@ package hu.bme.annaATbarbies.sokoban.view;
 
 import hu.bme.annaATbarbies.sokoban.model.Floor;
 import hu.bme.annaATbarbies.sokoban.model.pushable.Controller;
+import org.apache.log4j.Logger;
 
 import javax.swing.*;
 import java.util.Collections;
@@ -9,8 +10,17 @@ import java.util.List;
 
 public class ResultWindow extends JFrame {
 
+    private static final Logger logger = Logger.getLogger(ResultWindow.class);
+
     public void createAndShow() {
         List<Controller> workers = Floor.getInstance().getAllWorkers();
         Collections.sort(workers);
+
+        for (int i = 0; i < workers.size(); i++) {
+            Controller wor = workers.get(i);
+            logger.debug(String.format("%d. place: player%d with %d points.", i + 1, wor.getID(), wor.getPoint()));
+        }
+
+
     }
 }
